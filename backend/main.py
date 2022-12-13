8c1362a9d6239fcdc8d0a1211f23a04c2f59eb65
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2022-12-13 19:10:08 +0500

    add model/schema test

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from app.models.database import Base, engine
 
 from app.api.homework_router import media_router
 from app.api.space_router import space_router
-from app.api.test_router import test_router
+# from app.api.test_router import test_router
 from app.api.user_router import user_router
 
 from app.endpoints import signing
@@ -18,7 +18,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(media_router)
 app.include_router(space_router)
-app.include_router(test_router)
+# app.include_router(test_router)
 app.include_router(user_router)
 app.include_router(signing)
 app.include_router(spaces)
