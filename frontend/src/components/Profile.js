96734b8b269748eb50feacd4159980a6559742e5
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2022-12-23 03:41:03 +0500

    uploading user

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { AiOutlineUser } from "react-icons/ai"
+import axios from "axios"
 import user from "../img/user.png"
+import useToken from "./useToken";
+
+const token = sessionStorage.getItem('token')
+
+const config = {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+}
 
 class Profile extends React.Component {
-    user = [
-        {
-            "username": "nikrom",
-            "first_name": "Роман",
-            "last_name": "Никифоров",
-            "role": "Admin",
-            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            "hashed_password": "string",
-            "spaces": []
-        }
-    ]
+    constructor(props) {
+        super(props)
 
+        axios.get('http://localhost:8000/users/me', config).then((response) => {
+            this.setState({user: response.data})
+        })
+
+        this.state = {
+            user: []
+        }
+    }
+    
     render() {
         return (
-            <div>
-                {this.user.map((el) => (
-                    <div className="main-profile" key={el.id}>
-                        <a href="/#" className="profile-link">
-                        <img width="50px" height="50px" src={user}></img>
-                        </a>
-                        <p className="name-subject">
-                            <a href="/#" className="profile-link">{el.first_name} {el.last_name}</a>
-                        </p>
-                    </div>
-                ))}
-            </div>
+                <div className="main-profile" key={this.state.user.id}>
+                    <a href="/#" className="profile-link">
+                    <img width="50px" height="50px" src={user}></img>
+                    </a>
+                    <p className="name-subject">
+                        <a href="/#" className="profile-link">{this.state.user.first_name} {this.state.user.last_name}</a>
+                    </p>
+                </div>
         )
   }
 }
