637124549fce998de6351aee7687b90080711605
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2022-12-23 05:21:11 +0500

    spaces conn with db

--- frontend/src/pages/UserSpaces/SpacesPage.js
+++ frontend/src/pages/UserSpaces/SpacesPage.js
@@ -1,31 +1,38 @@
 import React from "react";
 import Header from "../../components/Header";
 import Spaces from "./Spaces";
+import axios from "axios"
 import AddSpace from "./AddSpace";
 import "./classesStyle.css"
 
+const token = sessionStorage.getItem('token')
+
+const config = {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+}
 
 class SpacesPage extends React.Component {
     constructor (props) {
         super(props)
+
+        axios.get('http://localhost:8000/space/?skip=0&limit=100', config).then((response) => {
+            this.setState({spaces: response.data})
+        })
+
         this.state = {
-            spaces: [
-                // {
-                //     title: "string",
-                //     description: "string",
-                //     id: 1,
-                //     modules: [],
-                //     users: []
-                // }
-            ]
+            spaces: []
         }
         this.addSpace = this.addSpace.bind(this)
     }
     
     addSpace(space) {
         console.log(space)
-        const id = this.state.spaces.length + 1
-        this.setState({spaces: [...this.state.spaces, {id, ...space}] })
+        axios.post('http://localhost:8000/space/', space, config).then((response) => {
+            this.setState({spaces: response.data})
+            console.log(response.data)
+        })
     }
     
     render () {
