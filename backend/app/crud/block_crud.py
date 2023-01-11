2b4cd19a55435c39362858e73b6d865544d471d8
Daniil <volkov.daniil.vit@ya.ru>
2023-01-12 01:37:51 +0500

    add put for blocks

--- backend/app/crud/block_crud.py
+++ backend/app/crud/block_crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 from app.models.blocks_models import TextMaterial as ModelTextMaterial
 from app.models.blocks_models import VideoMaterial as ModelVideoMaterial
-from app.schemas.block_schema import SchemaTextMaterialCreate, SchemaVideoCreate
+from app.schemas.block_schema import SchemaTextMaterialCreate, SchemaTextMaterialUpdate, SchemaVideoCreate, SchemaVideoUpdate
 
 
 def create_modules_text_materials(db: Session, text_material: SchemaTextMaterialCreate, module_id: int):
@@ -16,6 +16,19 @@ def create_modules_text_materials(db: Session, text_material: SchemaTextMaterial
 
 def get_text_material(db: Session, text_material_id: int):
     return db.query(ModelTextMaterial).filter(ModelTextMaterial.id == text_material_id).first()
+
+
+def update_text_material(db: Session, new_text_material_data: SchemaTextMaterialUpdate, text_material_id: int):
+    new_text_material = db.query(ModelTextMaterial).filter(ModelTextMaterial.id == text_material_id).first()
+
+    new_text_material.title = new_text_material_data.title
+    new_text_material.description = new_text_material_data.description
+    new_text_material.text = new_text_material_data.text
+
+    db.add(new_text_material)
+    db.commit()
+    db.refresh(new_text_material)
+    return new_text_material
 
 
 def delete_text_material(db: Session, text_material_id: int):
@@ -35,6 +48,19 @@ def create_modules_video(db: Session, video: SchemaVideoCreate, module_id: int):
 def get_video_material(db: Session, video_material_id: int):
     return db.query(ModelVideoMaterial).filter(ModelVideoMaterial.id == video_material_id).first()
 
+
+def update_video_material(db: Session, new_video_material_data: SchemaVideoUpdate, video_material_id: int):
+    new_video_material = db.query(ModelVideoMaterial).filter(ModelVideoMaterial.id == video_material_id).first()
+
+    new_video_material.title = new_video_material_data.title
+    new_video_material.description = new_video_material_data.description
+    new_video_material.url = new_video_material_data.url
+
+    db.add(new_video_material)
+    db.commit()
+    db.refresh(new_video_material)
+    return new_video_material
+    
 
 def delete_video_material(db: Session, video_material_id: int):
     db.query(ModelVideoMaterial).filter(ModelVideoMaterial.id == video_material_id).delete()
