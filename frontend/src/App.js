ebe8682188c1e56057326d103010b801b4f3b6d1
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2023-01-13 02:35:22 +0500

    add navigate from authpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,9 @@ function App() {
 
     //Закомментировать для запуска через npm 
 
-    if(!token) {
-        return <AuthPage setToken={setToken} />
-    }
+    // if(!token) {
+    //     return <AuthPage setToken={setToken} />
+    // }
     
     return ( 
         <BrowserRouter>
@@ -24,7 +24,7 @@ function App() {
                 <Route path="/userspaces" element={<SpacesPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/space" element={<SpacePage />} />
-                <Route path="/login" element={<AuthPage />} />
+                <Route path="/login" element={<AuthPage setToken={setToken}/>} />
                 <Route path="/classTeacher" element={<ClassTeacher />} />
             </Routes>
         </BrowserRouter>
